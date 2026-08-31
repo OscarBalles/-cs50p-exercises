5b1ce5a801740d4264d7aa9dc3577f8f4f956ed9
Oscar <oballesteros192@gmail.com>
2026-08-31 14:20:10 -0500

    :wq
    c:/Users/oball/OneDrive/Documentos/Data/-cs50p-exercises/hola.py:wq
    
    :wq

--- hola.py
+++ hola.py
@@ -1,1 +1,9 @@
-print("Hola mundo")
+x = 2
+y = 3
+def main():
+    input("Hola, como estas?")
+    name = input("¿Cuál es tu nombre? ")
+    print(f"¡Hola, {name}!")
+    if input("1+1=") == "x":print("correcto"), print("incorrecto")
+
+main()
